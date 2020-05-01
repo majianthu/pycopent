335c0d8c3d4e31966ef480145f0968c1ee28875a
MA Jian <62880254+majianthu@users.noreply.github.com>
2020-05-01 15:21:11 +0800

    Create test.py

---
+++ tests/test.py
@@ -0,0 +1,14 @@
+from numpy.random import multivariate_normal as mnorm
+from numpy import corrcoef
+from math import log 
+import numpy as np
+import copent
+
+rho = 0.8
+mean1 = [0,0]
+cov1 = [ [1,rho],[rho,1] ]
+data = mnorm(mean1, cov1, 500) # bivariate gaussian 
+truevalue1 =  - log(1 - rho**2) / 2  # true value of copula entropy
+copent1 = copent.copent(data) # estimated copula entropy
+str1 = "true value: %0.3f; Estimated CE: %0.3f" % (truevalue1, copent1)
+print(str1)
