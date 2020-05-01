fc43d96cdd7ca84b556d6f33dfb4fd58a1fa8b06
MA Jian <62880254+majianthu@users.noreply.github.com>
2020-05-01 15:24:23 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="copent",
+    version="0.1.4",
+    author="MA Jian",
+    author_email="majian03@gmail.com",
+    description="Estimating Copula Entropy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license = 'GPL License',
+    url="https://github.com/majianthu/pycopent",
+    packages=setuptools.find_packages(),
+    install_requires = ['numpy','scipy'],
+    python_requires='>=2.7',
+)
