32568a0aa72e6709bed05473a570d1c4c9b5513c
MA Jian <majian1979@qq.com>
2022-01-26 03:05:55 +0800

    add argument for running mode

--- copent/copent.py
+++ copent/copent.py
@@ -1,14 +1,14 @@
 ##################################################################################
 ###  Estimating Copula Entropy and Transfer Entropy 
-###  2021-10-18
+###  2022-01-25
 ###  by Ma Jian (Email: majian03@gmail.com)
 ###
 ###  Parameters
 ###	x    	: N * d data, N samples, d dimensions
-###	k    	: kth nearest neighbour, parameter for kNN entropy estimation 
-###	dtype	: distance type [1: 'Euclidean', others: 'Maximum distance']
-###	lag	: time lag
-###	log0 	: for log0 error
+###	k    	: kth nearest neighbour, parameter for kNN entropy estimation. default = 3
+###	dtype	: distance type [1: 'Euclidean', 2/others (default): 'Maximum distance']
+###	mode	: running mode, 1(default): for speed/small data, 2: for space/large data
+###	lag	: time lag. default = 1
 ###
 ###  References
 ###  [1] Ma Jian, Sun Zengqi. Mutual information is copula entropy. 
@@ -40,18 +40,24 @@ def dist(x, dtype = 2):
 				xd[j,i] = xd[i,j]
 	
 	return xd
+##### calculating the distance between two samples
+def dist_ij(xi,xj, dtype = 2):
+	if dtype == 1:
+		return sqrt(sum(square(xi-xj)))
+	else: ## dtype = 2
+		return max(abs(xi-xj))
 
 ##### constructing empirical copula density [1]
 def construct_empirical_copula(x):
 	(N,d) = x.shape	
-	xc = zeros([N,d])
+	xc = zeros([N,d]) 
 	for i in range(0,d):
 		xc[:,i] = rank(x[:,i]) / N
 	
 	return xc
 
 ##### Estimating entropy with kNN method [2]
-def entknn(x, k = 3, dtype = 2):
+def entknn(x, k = 3, dtype = 2, mode = 1):
 	(N,d) = x.shape
 	
 	g1 = digamma(N) - digamma(k)
@@ -61,16 +67,24 @@ def entknn(x, k = 3, dtype = 2):
 	else:	# maximum distance
 		cd = 1;
 	
-	distx = dist(x, dtype)		
 	logd = 0
-	for i in range(0,N):
-		distx[i,:].sort()
-		logd = logd + log( 2 * distx[i,k] ) * d / N
+	if mode == 1: # for speed / small data
+		distx = dist(x, dtype)		
+		for i in range(0,N):
+			distx[i,:].sort()
+			logd = logd + log( 2 * distx[i,k] ) * d / N
+	else: # 2, for space / large data
+		for i in range(0,N):
+			disti = []
+			for j in range(0,N):
+				disti.append( dist_ij(x[i,:],x[j,:],dtype) )
+			disti.sort()
+			logd = logd + log( 2 * disti[k] ) * d / N
 
 	return (g1 + log(cd) + logd)
 
 ##### 2-step Nonparametric estimation of copula entropy [1]
-def copent(x, k = 3, dtype = 2, log0 = False):
+def copent(x, k = 3, dtype = 2, mode = 1, log0 = False):
 	xarray = array(x)
 
 	if log0:
@@ -85,20 +99,21 @@ def copent(x, k = 3, dtype = 2, log0 = False):
 	xc = construct_empirical_copula(xarray)
 
 	try:
-		return -entknn(xc, k, dtype)
+		return -entknn(xc, k, dtype, mode)
 	except ValueError: # log0 error
-		return copent(x, k, dtype, log0 = True)
+		return copent(x, k, dtype, mode, log0 = True)
+
 
 ##### conditional independence test [3]
 ##### to test independence of (x,y) conditioned on z
-def ci(x, y, z, k = 3, dtype = 2):
+def ci(x, y, z, k = 3, dtype = 2, mode = 1):
 	xyz = vstack((x,y,z)).T
 	yz = vstack((y,z)).T
 	xz = vstack((x,z)).T
-	return copent(xyz,k,dtype) - copent(yz,k,dtype) - copent(xz,k,dtype)
+	return copent(xyz,k,dtype,mode) - copent(yz,k,dtype,mode) - copent(xz,k,dtype,mode)
 
 ##### estimating transfer entropy from y to x with lag [3]
-def transent(x, y, lag = 1, k = 3, dtype = 2):
+def transent(x, y, lag = 1, k = 3, dtype = 2, mode = 1):
 	xlen = len(x)
 	ylen = len(y)
 	if (xlen > ylen):
@@ -110,4 +125,4 @@ def transent(x, y, lag = 1, k = 3, dtype = 2):
 	x1 = x[0:(l-lag)]
 	x2 = x[lag:l]
 	y = y[0:(l-lag)]
-	return ci(x2,y,x1,k,dtype)
+	return ci(x2,y,x1,k,dtype,mode)
