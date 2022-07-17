d1cc2d1ffd97a6f64ac16a68edc82041936d4b88
MA Jian <majian1979@qq.com>
2022-07-17 15:06:12 +0800

    Update test1ce.py

--- tests/test1ce.py
+++ tests/test1ce.py
@@ -5,7 +5,7 @@ import copent
 rho = 0.8
 mean1 = [0,0]
 cov1 = [ [1,rho],[rho,1] ]
-data = mnorm(mean1, cov1, 500) # bivariate gaussian 
+data = mnorm(mean1, cov1, 800) # bivariate gaussian 
 truevalue1 =  - log(1 - rho**2) / 2  # true value of copula entropy
 copent1 = copent.copent(data) # estimated copula entropy
 str1 = "true value: %0.3f; Estimated CE: %0.3f" % (truevalue1, copent1)
